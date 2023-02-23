98fe738039d509875eb16f628960802989009c0f
hitansiv.tagline@gmal.com <hitansiv.tagline@gmail.com>
2023-02-23 16:24:30 +0530

    optimized code

--- Django_Blog/users/urls.py
+++ Django_Blog/users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import sign_up,sign_in,sign_out
+from .views import Sign_up,Sign_in,sign_out
 urlpatterns = [
-    path('',sign_up,name='sign_up'),
-    path('signin/',sign_in,name='sign_in'),
-    path('signout/',sign_out,name='sign_out')
-    
+    path('',Sign_up.as_view(),name='sign_up'),
+    path('signin/',Sign_in.as_view(),name='sign_in'),
+    path('signout/',sign_out,name='sign_out')   
 ]
--- Django_Blog/users/views.py
+++ Django_Blog/users/views.py
@@ -3,37 +3,41 @@ from .form import Signup
 from django.contrib import messages
 from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.forms import AuthenticationForm 
+from django.views.generic import View
 
-# signup function
-def sign_up(request):
-    if request.method == "POST":
+# signup 
+class Sign_up(View):
+    template_name = 'signup.html'
+    def post(self,request):
         form = Signup(request.POST)
         if form.is_valid():
             user = form.save()
             login(request,user)
             return HttpResponseRedirect("signin/")
-    form = Signup()
-    return render(request,'signup.html',{'signup_form':form})
+        return render(request,self.template_name,{'signup_form':form})
+    def get(self,request):
+        form = Signup()
+        return render(request,self.template_name,{'signup_form':form})
 
-#login function
-def sign_in(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request=request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				return HttpResponseRedirect("/blog/")
-		# 	else:
-		# 		messages.error(request,"Invalid username or password.")
-		# else:
-		# 	messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request,"login.html",{"login_form":form})
+#login 
+class Sign_in(View):
+    template_name = 'login.html'
+    def post(self, request):
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect("/blog/")
+        return render(request,self.template_name,{'login_form':form})
+        
+    def get(self,request):
+        form = AuthenticationForm()
+        return render(request,self.template_name,{"login_form":form})
 
-#logout function
+#logout
 def sign_out(request):
     logout(request)
     return HttpResponseRedirect('/accounts/signin/')
