aa531a9222bd4c04045ef42ca8d40af8419fbf62
hitansiv.tagline@gmal.com <hitansiv.tagline@gmail.com>
2023-03-06 10:40:52 +0530

    Add testcases for check blog content or total blog

--- Django_Blog/blog/urls.py
+++ Django_Blog/blog/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
-from .views import IndexView,AuthorListView,AuthorDetailView,BlogListView,BlogDetailView,commentview,AuthorForm,BlogForm
+from .views import (
+    IndexView,
+    AuthorListView,
+    AuthorDetailView,
+    BlogListView,
+    BlogDetailView,
+    CommentView,
+    AuthorForm,
+    BlogForm,
+)
+
 urlpatterns = [
-    path('',IndexView.as_view(),name='index'),
-    path('blogger/',AuthorListView.as_view(),name='authorlist'),
-    path('blogger/<int:pk>/',AuthorDetailView.as_view(),name='authordetail'),
-    path('blogger/form/',AuthorForm.as_view(),name='authorform'),
-    path('blogs/',BlogListView.as_view(),name='blogs'),
-    path('blogs/<int:pk>/',BlogDetailView.as_view(),name='blogdetail'),
-    path('blogs/<int:pk>/create/',commentview,name='comment'),
-    path('blogs/form/',BlogForm.as_view(),name='blogform'),
+    path("", IndexView.as_view(), name="index"),
+    path("blogger/", AuthorListView.as_view(), name="authorlist"),
+    path("blogger/<int:pk>/", AuthorDetailView.as_view(), name="authordetail"),
+    path("blogger/form/", AuthorForm.as_view(), name="authorform"),
+    path("blogs/", BlogListView.as_view(), name="blogs"),
+    path("blogs/<int:pk>/", BlogDetailView.as_view(), name="blogdetail"),
+    path("blogs/<int:pk>/create/", CommentView.as_view(), name="comment"),
+    path("blogs/form/", BlogForm.as_view(), name="blogform"),
 ]
