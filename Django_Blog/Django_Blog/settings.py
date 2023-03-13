1a9728eb701ec84a91b9cd5b4a245df25e9f2658
hitansiv.tagline@gmal.com <hitansiv.tagline@gmail.com>
2023-03-13 09:28:25 +0530

    Add secret variable in github secrets

--- Django_Blog/Django_Blog/settings.py
+++ Django_Blog/Django_Blog/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cm58)$t-pzx=k#win@y283)+qzzul3r%zx)t!-7z3$qb2=++^5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +82,7 @@ WSGI_APPLICATION = 'Django_Blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  BASE_DIR / 'db.sqlite3',
+        'NAME':  os.getenv('DATABASE_NAME'),
     }
 }
 
